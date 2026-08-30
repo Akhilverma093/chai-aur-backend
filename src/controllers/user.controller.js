fac2e326883b820c6d151f37331ca8242f1d7488
Akhilverma093 <akhilver093@gmail.com>
2026-08-31 02:37:43 +0530

    add cloudinary deleted file

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
-import { uploadOnCloudinary } from "../utils/coludinary.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 
@@ -128,7 +128,7 @@ const loginUser=asyncHandler(async (req,res)=>{
 
   const {email,username,password}=req.body;
 
-  if(!username || !email){
+  if(!username && !email){
     throw new ApiError(400,"username or email is required")  
   }
 
